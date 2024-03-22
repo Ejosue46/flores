fdca2c83e572a42342c7cff5015e529a5a87a4da
Ejosue46 <124466405+Ejosue46@users.noreply.github.com>
2024-03-21 22:20:36 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const flowers = document.querySelectorAll(".flower");
-    flowers.forEach((flower, index) => {
-      setInterval(() => {
-        flower.style.transform = "rotate(45deg)";
-        setTimeout(() => {
-          flower.style.transform = "rotate(0deg)";
-        }, 1000);
-      }, 2000 + index * 500);
+document.addEventListener("DOMContentLoaded", () => {
+        const flowers = document.querySelectorAll(".flower");
+        flowers.forEach((flower, index) => {
+            setInterval(() => {
+                flower.style.transform = "rotate(45deg)";
+                setTimeout(() => {
+                    flower.style.transform = "rotate(0deg)";
+                }, 1000);
+            }, 2000 + index * 500);
+        });
     });
-  });
   
